452a68314d7ec2081d76b7ed83868d22dc5da4ac
flaryx32 <77863658+flaryx32@users.noreply.github.com>
2024-02-09 19:28:52 +0100

    Create script.js

---
+++ static/script.js
@@ -0,0 +1,7 @@
+document.getElementById('fetchNew').addEventListener('click', function() {
+    window.location.href = '/fetch_new';
+});
+
+document.getElementById('refresh').addEventListener('click', function() {
+    window.location.href = '/refresh';
+});
